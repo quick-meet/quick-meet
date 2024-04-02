a4a572c308bb4df2b5b812bf13a3344a1c9bf1be
makar-pelogeiko <m.pelogeiko@mail.ru>
2024-04-02 15:54:57 +0300

    [spring web app init]

---
+++ back-end/quick-meet/src/main/java/ru/imo/quickmeet/QuickMeetApplication.java
@@ -0,0 +1,13 @@
+package ru.imo.quickmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickMeetApplication.class, args);
+	}
+
+}
---
+++ back-end/quick-meet/src/test/java/ru/imo/quickmeet/QuickMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.imo.quickmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
